2d9260b0ce9d5f69431c13023d4b96cc87c3203d
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-04-21 18:18:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from nextcord import Intents
 bot = commands.Bot(command_prefix=['c!'], intents=Intents.all(), help_command=None)
 @bot.command()
 async def reload(ctx):
-  for fn in os.listdir("./cogs"): 
-    if fn.endswith('.py'): bot.reload_extension(f'cogs.{fn[: -3]}')
+  for fn in os.listdir("./cogs"): bot.reload_extension(f'cogs.{fn[: -3]}') if fn.endswith('.py') else print('no code')
   await ctx.send('Reloaded mom')
-for fn in os.listdir("./cogs"): 
-  if fn.endswith('.py'): bot.load_extension(f'cogs.{fn[: -3]}')
+for fn in os.listdir("./cogs"): bot.load_extension(f'cogs.{fn[: -3]}') if fn.endswith('.py') else print('no code')
 bot.run(os.environ['token'])
